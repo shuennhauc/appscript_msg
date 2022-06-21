213f2580b8a10a6133b08043761fccd2a8467580
shuennhauc <54190149+shuennhauc@users.noreply.github.com>
2022-06-21 11:04:26 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='appscript_msg',
+    version=0.1.0,
+    description='Outgoing email automation',
+  
+    url='https://github.com/shuennhauc/appscript_msg',
+    author='Shuennhau Chang',
+    author_email='shuennhauc@gmail.com',
+
+    install_requires=['appscript']
+  
+)
