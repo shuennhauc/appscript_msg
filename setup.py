3d8fe1e09e3dff89ee6c3126d7ff852069768ccc
shuennhauc <54190149+shuennhauc@users.noreply.github.com>
2022-06-21 11:16:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='appscript_msg',
-    version=0.1.0,
+    version=1.0,
     description='Outgoing email automation',
   
     url='https://github.com/shuennhauc/appscript_msg',
